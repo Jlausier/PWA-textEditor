76ee13692d3f0e0ddc15d60cee3648e6efb6d8ab
Jacob <jakelausier7878@gmail.com>
2023-08-24 22:58:20 -0400

    got base code need to test for errors

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,27 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault(); // Prevent the default installation prompt
+    const installButton = document.getElementById('buttonInstall');
+    installButton.style.display = 'block'; // Show your custom install butto
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    installButton.style.display = 'none'; // Hide the custom install button
+    e.prompt(); // Show the native installation prompt
+    const choiceResult = await event.userChoice;
+    if (choiceResult.outcome === 'accepted') {
+      console.log('User accepted the installation');
+    } else {
+      console.log('User dismissed the installation');
+    }
+  });
+
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('App was successfully installed');
+    // You can perform additional actions here, such as showing a thank you message
+});
